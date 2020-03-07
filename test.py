e8b660b2280d3d341a4349f3a0d7419ce510b720
StevenKiT <544736192@qq.com>
2020-03-06 21:10:10 -0600

    第一个python的学习

---
+++ test.py
@@ -0,0 +1,77 @@
+# #!/usr/bin/python
+# # -*- coding: UTF-8 -*-
+#
+# 建立变量
+# one: int = 3
+# two: int = 4
+# three: int = 5
+#
+#
+# 这是用"\"隔开表示分行
+# total = one + \
+#         two + \
+#         three
+#
+# print(total)
+#
+#
+# from pip._vendor.distlib.compat import raw_input
+#
+# 用户输入类----------对应java的Scanner
+# num = raw_input("输入\n");
+# print(num)
+#
+#
+#
+#
+
+# 这个是多行并列，python可以一行列很多，用分号";"隔开即可
+# import sys;x = 'womanise'; sys.stdout.write(x + '\n')
+
+
+# 这个是换行输出
+# x = 'a'
+# y = """vb"""
+# print(x)
+# print(y)
+
+# 输出字符串
+# str = 'queened'
+#
+# print(*str[1:] * 2 + "fn" * 2)
+
+# 从开始到跳行位置结束，打印首字母和尾字母
+# letters = ['c', 'h', 'e', 'c', 'k', 'i', 'o']
+#
+# print(letters[0:5:2])
+
+# list = ['rundoc', 786, 2.23, 'john', 70.2]
+# tinylist = [123, 'john']
+#
+
+# 默认不打印最后一个元素
+# print(list)  # 输出完整列表
+# print(list[0])  # 输出列表的第一个元素
+# print(list[1:3])  # 输出第二个至第三个元素
+# print(list[2:])  # 输出从第三个开始至列表末尾的所有元素
+# print(tinylist * 2)  # 输出列表两次
+# print(list + tinylist)  # 打印组合的列表
+
+# 默认不打印最后一个元素
+# list = ['0', '1', '2', '3']
+# print(list[0:3])
+
+
+# List是同过偏移值来存储的
+# 而字典"dict"是通过索引(key)关键字来存储信息的的。
+# 在应用来说两者使用情况不太一样。
+dict = {'one': "this is one", 2: "this is two"}
+tinydict = {'name': 'john', 'code': 6734, 'dept': 'sales'}
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
+print(tinydict['name'])
+
+
